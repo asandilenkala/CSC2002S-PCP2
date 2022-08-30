4ee1e9c241577ed716ef7215a7182a29f6005571
Asandile Nkala <nklasa001@myuct.ac.za>
2022-08-30 15:08:04 +0200

    Updating edits

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -7,84 +7,86 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements Runnable {
-		private AtomicBoolean done ; //REMOVE
-		private AtomicBoolean started ; //REMOVE
-		private AtomicBoolean won ; //REMOVE+
+	private AtomicBoolean done; // REMOVE
+	private AtomicBoolean started; // REMOVE
+	private AtomicBoolean won; // REMOVE+
 
-		private CrossingWord[] crossingWords; //**************************************
-		private FallingWord[] words;
-		private int noWords;
-		private final static int borderWidth=25; //appearance - border
-        private final static int borderHeight=25; //************************************
+	private CrossingWord[] crossingWords;
+	private CrossingWord crossingWord;
+	private FallingWord[] words;
+	private int noWords;
+	private final static int borderWidth = 25; // appearance - border
+	private final static int borderHeight = 25;
 
-		GamePanel(FallingWord[] words, int maxY,	
-				 AtomicBoolean d, AtomicBoolean s, AtomicBoolean w, CrossingWord[] crossingWords) { //********************************
-			this.words=words; //shared word list
-			this.crossingWords = crossingWords; //********************************************
-			noWords = words.length; //only need to do this once
-			done=d; //REMOVE
-			started=s; //REMOVE
-			won=w; //REMOVE
-		}
-		
-		public void paintComponent(Graphics g) {
-		    int width = getWidth()-borderWidth*2;
-		    int height = getHeight()-borderWidth*2;
-		    g.clearRect(borderWidth,borderWidth,width,height);//the active space
-		    g.setColor(Color.pink); //change colour of pen
-		    g.fillRect(borderWidth,height,width,borderWidth); //draw danger zone
-
-		    g.setColor(Color.black);
-		    g.setFont(new Font("Arial", Font.PLAIN, 26));
-		   //draw the words
-		    if (!started.get()) {
-		    	g.setFont(new Font("Arial", Font.BOLD, 26));
-				g.drawString("Type all the words before they hit the red zone,press enter after each one.",borderWidth*2,height/2);	
-		    	
-		    }
-		    else if (!done.get()) {
-		    	for (int i=0;i<noWords;i++){	    	
-		    		g.drawString(words[i].getWord(),words[i].getX()+borderWidth,words[i].getY());	//*******************************************
-		    	}
-				int position = 2;
-				g.drawString(crossingWords[position].getWord(), crossingWords[position].getX()+borderWidth,crossingWords[position].getY()); //************
-		    	
-				g.setColor(Color.lightGray); //change colour of pen
-		    	g.fillRect(borderWidth,0,width,borderWidth);
-		   }
-		   else { if (won.get()) {
-			   g.setFont(new Font("Arial", Font.BOLD, 36));
-			   g.drawString("Well done!",width/3,height/2);	
-		   } else {
-			   g.setFont(new Font("Arial", Font.BOLD, 36));
-			   g.drawString("Game over!",width/2,height/2);	
-		   }
-		   }
-		}
-		
-		public int getValidXpos() {
-			int width = getWidth()-borderWidth*4;
-			int x= (int)(Math.random() * width);
-			return x;
-		}
-
-		public int getValidYpos() {   //**********************************************************
-			int height = getHeight()-borderHeight*4;
-			int y= (int)(Math.random() * height);
-			return y;
-		}
-		
-		public void run() {
-			while (true) {
-				repaint();
-				try {
-					Thread.sleep(10); 
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				};
-			}
-		}
-
+	GamePanel(FallingWord[] words, int maxY,
+			AtomicBoolean d, AtomicBoolean s, AtomicBoolean w, CrossingWord crossingWord) {
+		this.crossingWord = crossingWord;
+		noWords = words.length; // only need to do this once
+		this.words = words;
+		done = d; // REMOVE
+		started = s; // REMOVE
+		won = w; // REMOVE
 	}
 
+	public void paintComponent(Graphics g) {
+		int width = getWidth() - borderWidth * 2;
+		int height = getHeight() - borderWidth * 2;
+		g.clearRect(borderWidth, borderWidth, width, height);// the active space
+		g.setColor(Color.pink); // change colour of pen
+		g.fillRect(borderWidth, height, width, borderWidth); // draw danger zone
 
+		g.setColor(Color.black);
+		g.setFont(new Font("Arial", Font.PLAIN, 26));
+		// draw the words
+		if (!started.get()) {
+			g.setFont(new Font("Arial", Font.BOLD, 26));
+			g.drawString("Type all the words before they hit the red zone,press enter after each one.", borderWidth * 2,
+					height / 2);
+
+		} else if (!done.get()) {
+			for (int i = 0; i < noWords; i++) {
+				g.drawString(words[i].getWord(), words[i].getX() + borderWidth, words[i].getY()); // *******************************************
+			}
+			int position = 2;
+			g.setColor(Color.green);
+			g.drawString(crossingWord.getWord(), crossingWord.getX() + borderWidth,
+					crossingWord.getY());
+
+			g.setColor(Color.lightGray); // change colour of pen
+			g.fillRect(borderWidth, 0, width, borderWidth);
+		} else {
+			if (won.get()) {
+				g.setFont(new Font("Arial", Font.BOLD, 36));
+				g.drawString("Well done!", width / 3, height / 2);
+			} else {
+				g.setFont(new Font("Arial", Font.BOLD, 36));
+				g.drawString("Game over!", width / 2, height / 2);
+			}
+		}
+	}
+
+	public int getValidXpos() {
+		int width = getWidth() - borderWidth * 4;
+		int x = (int) (Math.random() * width);
+		return x;
+	}
+
+	public int getValidYpos() { // **********************************************************
+		int height = getHeight() - borderHeight * 4;
+		int y = (int) (Math.random() * height);
+		return y;
+	}
+
+	public void run() {
+		while (true) {
+			repaint();
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			;
+		}
+	}
+
+}
--- src/HungryWordMover.java
+++ src/HungryWordMover.java
@@ -4,54 +4,57 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class HungryWordMover extends Thread {
 	private CrossingWord myWord;
 	private AtomicBoolean done;
-	private AtomicBoolean pause; 
+	private AtomicBoolean pause;
 	private Score score;
-	CountDownLatch startLatch; //so all can start at once
-	
-	HungryWordMover( CrossingWord word) {
+	CountDownLatch startLatch; // so all can start at once
+
+	HungryWordMover(CrossingWord word) {
 		myWord = word;
 	}
-	
-	HungryWordMover( CrossingWord myWord,WordDictionary dict, Score score,
+
+	HungryWordMover(CrossingWord myWord, WordDictionary dict, Score score,
 			CountDownLatch startLatch, AtomicBoolean d, AtomicBoolean p) {
-		this.myWord=myWord;
+		this.myWord = myWord;
 		this.startLatch = startLatch;
-		this.score=score;
-		this.done=d;
-		this.pause=p;
+		this.score = score;
+		this.done = d;
+		this.pause = p;
 	}
-	
-	
-	
+
 	public void run() {
 
-		//System.out.println(myWord.getWord() + " falling speed = " + myWord.getSpeed());
+		// System.out.println(myWord.getWord() + " falling speed = " +
+		// myWord.getSpeed());
 		try {
-			System.out.println(myWord.getWord() + " waiting to start " );
+			System.out.println(myWord.getWord() + " hungry waiting to start ");
 			startLatch.await();
 		} catch (InterruptedException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		} //wait for other threads to start
-		System.out.println(myWord.getWord() + " started" );
-		while (!done.get()) {				
-			//animate the word
-			while (!myWord.crossed() && !done.get()) {  //*******************************
-				    myWord.moveRight(20);
-					try {
-						sleep(myWord.getSpeed());
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					};		
-					while(pause.get()&&!done.get()) {};
+		} // wait for other threads to start
+		System.out.println(myWord.getWord() + " started");
+		while (!done.get()) {
+			// animate the word
+			while (!myWord.crossed() && !done.get()) { // *******************************
+				myWord.moveRight(20);
+				System.out.println(myWord.getWord() + " " + myWord.getX() + " y : " + myWord.getY());
+				try {
+					sleep(myWord.getSpeed());
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				;
+				while (pause.get() && !done.get()) {
+				}
+				;
 			}
-			if (!done.get() && myWord.crossed()) {  //*******************************************
+			if (!done.get() && myWord.crossed()) { // *******************************************
 				score.missedWord();
 				myWord.resetWord();
 			}
 			myWord.resetWord();
 		}
 	}
-	
+
 }
